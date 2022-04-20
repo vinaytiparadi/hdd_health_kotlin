43d5e68f73d90a4d37d6183f8d58fe7f85f60ce9
Vinay Tiparadi <vinaytiparadi123@gmail.com>
2022-04-20 21:58:20 +0530

    initialize app with material3 theming

---
+++ app/src/main/java/tech/vinay/hddhealth/MainActivity.kt
@@ -0,0 +1,12 @@
+package tech.vinay.hddhealth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import tech.vinay.hddlifehealth.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
