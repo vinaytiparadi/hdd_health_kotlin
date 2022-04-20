8e52738760a1821f3e8c07c136ce875e14b6c8c3
Vinay Tiparadi <vinaytiparadi123@gmail.com>
2022-04-20 23:21:31 +0530

    design ui

--- app/src/main/java/tech/vinay/hddhealth/MainActivity.kt
+++ app/src/main/java/tech/vinay/hddhealth/MainActivity.kt
@@ -1,8 +1,7 @@
 package tech.vinay.hddhealth
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import tech.vinay.hddlifehealth.R
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
