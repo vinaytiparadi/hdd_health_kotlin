044f589063fe6d54906d020530527730da68c848
Vinay Tiparadi <vinaytiparadi123@gmail.com>
2022-04-30 13:06:24 +0530

    calculate and display status symbol and message

--- app/src/main/java/tech/vinay/hddhealth/MainActivity.kt
+++ app/src/main/java/tech/vinay/hddhealth/MainActivity.kt
@@ -16,6 +16,8 @@ class MainActivity : AppCompatActivity() {
         val hours_edit_text = findViewById<TextInputEditText>(R.id.hours_edit_text)
         val check_button = findViewById<ExtendedFloatingActionButton>(R.id.check_button)
         val health_message_text_view = findViewById<MaterialTextView>(R.id.health_message_text_view)
+        val status_message_text_view = findViewById<MaterialTextView>(R.id.status_message_text_view)
+        val replace_text_view = findViewById<MaterialTextView>(R.id.replace_text_view)
 
         check_button.setOnClickListener {
             val text = hours_edit_text.text.toString()
@@ -26,14 +28,31 @@ class MainActivity : AppCompatActivity() {
 
             val df = DecimalFormat("#.##")
             df.roundingMode = RoundingMode.DOWN
-            val monthsRoundOff = df.format(months)
-            val yearsRoundOff = df.format(years)
+            val monthsRoundOff = df.format(months).toFloat()
+            val yearsRoundOff = df.format(years).toFloat()
 
-            println(text)
-            health_message_text_view.text = "Bruh, your HDD has been been active for about ${monthsRoundOff} months! + ${yearsRoundOff}"
+            val yearsToReplace=5-yearsRoundOff
+
+
+            health_message_text_view.text = "Heyy, your HDD has been been active for about ${monthsRoundOff} months!!"
+
+            if(yearsRoundOff>=0 && yearsRoundOff<=1.5){
+                status_message_text_view.text="Status: 🟢 (Healthy AF)"
+            }
+            else if(yearsRoundOff>=1.5 && yearsRoundOff<=3.5){
+                status_message_text_view.text="Status: 🟠 (Good)"
+            }
+            else if(yearsRoundOff>3.5){
+                status_message_text_view.text="Status: 🔴 (Severe)"
+            }
+
+            replace_text_view.text = "You should prefer to replace your HDD after $yearsToReplace of active time."
+
+
             return@setOnClickListener
     }
     }
 }
+
 
 
